1f8c99f270ceea28e43332dbe49b6a91c0b7a80c
Desenvolvedor SSG05 <joaomgsa@gmail.com>
2023-11-29 17:50:16 -0300

    Formulario de Cadastro

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gymapp/pages/authentication_tela.dart';
 import 'package:flutter_gymapp/pages/exercicio_tela.dart';
 
 void main() {
@@ -11,9 +12,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return const MaterialApp(
 
-      home: ExercicioTela(),
+      home: AuthenticationTela(),
     );
   }
 }
--- lib/pages/authentication_tela.dart
+++ lib/pages/authentication_tela.dart
@@ -1,10 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gymapp/_core/my_colors.dart';
 
-class AuthenticationTela extends StatelessWidget {
+class AuthenticationTela extends StatefulWidget {
   const AuthenticationTela({super.key});
 
   @override
+  State<AuthenticationTela> createState() => _AuthenticationTelaState();
+}
+
+class _AuthenticationTelaState extends State<AuthenticationTela> {
+  bool _isLogin = true;
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: MyColors.azulEscuro,
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  MyColors.azulTopoGradiente,
+                  MyColors.azulBaixoGradiente
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight
+              )
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Form(
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children:[
+                     Image.asset("assets/logo.png",height: 128),
+                      const Text(
+                          "Meu Maromba",
+                          textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 48,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white
+                      ),),
+                      const SizedBox(height: 32,),
+                      TextFormField(decoration: const InputDecoration(label: Text("E-mail")),),
+                      TextFormField(decoration: const InputDecoration(label: Text("Senha")),obscureText: true,),
+                     Visibility(visible : !_isLogin, child: Column(
+                        children: [
+                          TextFormField(decoration: const InputDecoration(label: Text("Confirmar Senha")),obscureText: true,),
+                          TextFormField(decoration: const InputDecoration(label: Text("Nome ")),),
+                        ],
+                     )),
+                      const SizedBox(height: 16,),
+                      ElevatedButton(onPressed: () {}, child: Text((_isLogin)? "Entrar": "Cadastrar"),),
+                      Divider(),
+                      TextButton(onPressed: () {
+                        setState(() {
+                          _isLogin = !_isLogin;
+                        });
+                      }, child: Text((_isLogin)? "Não tem conta? Clique aqui!": "Já tem uma conta? Entre"),),
+                   ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      )
+    );
   }
 }
